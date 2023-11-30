44fb93ec77a23c9f331508800a1b20f6dd26997f
Kinsteen <me@kinsteen.fr>
2023-11-30 21:38:59 +0100

    Make superclass Building, make it so we can't place a building on top of another

--- scenes/GameScene.gd
+++ scenes/GameScene.gd
@@ -15,4 +15,9 @@ func _process(delta):
 func _on_button_pressed():
 	var canon_inst = canon.instantiate()
 	canon_inst.set_scale(Vector2(2, 2))
+	canon_inst.can_place = func (this):
+		for node in get_tree().get_nodes_in_group("building"):
+			if node != this and node.position == this.position:
+				return false
+		return true
 	self.add_child(canon_inst)
---
+++ scripts/Building.gd
@@ -0,0 +1,53 @@
+class_name Building extends Node2D
+
+enum State {
+	IDLE,
+	MOVING
+	# Could create more state here, like attacking, broken maybe?
+}
+
+var can_place: Callable = func (this: Building):
+	return true
+
+signal state_changed(old: State, new: State)
+var state: State:
+	set(new_state):
+		state_changed.emit(state, new_state)
+		state = new_state
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	state = State.MOVING
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if not can_place.call(self):
+		$Area2D/Sprite2D.set_modulate(Color(1, .5, .5, 0.8))
+	else:
+		self._on_state_changed(state, state)
+
+# This is triggered everywhere everytime?
+func _input(event):
+	if event is InputEventMouseMotion and state == State.MOVING:
+		var snap := 16*3
+		var offset := 16 + 16/2
+		self.position = Vector2(
+			int(event.position.x / snap) * snap + offset,
+			int(event.position.y / snap) * snap
+		)
+	if event is InputEventMouseButton and event.is_pressed():
+		if state == State.MOVING and can_place.call(self):
+			state = State.IDLE
+			get_viewport().set_input_as_handled() # Stop propagating the event
+
+func _on_state_changed(old, new):
+	if new == State.MOVING:
+		$Area2D/Sprite2D.set_modulate(Color(1, 1, 1, 0.3))
+	elif new == State.IDLE:
+		$Area2D/Sprite2D.set_modulate(Color(1, 1, 1, 1))
+
+# This is only when clicking in the collision shape
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed():
+		if state == State.IDLE and can_place.call(self): # TODO dirty, should remember when we're moving something?
+			state = State.MOVING
