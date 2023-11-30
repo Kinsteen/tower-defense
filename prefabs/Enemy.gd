441deb7b42b23936b1ef08aef296474bcffcd7a5
PaulLuperini <paul.luperini@gmail.com>
2023-11-30 23:04:45 +0100

    add a basic Goal
    The enemy now goes towards the goal.
    Once it reaches it, it inflicts damage.
    
    Closes #6

--- prefabs/Enemy.gd
+++ prefabs/Enemy.gd
@@ -1,15 +1,28 @@
 extends CharacterBody2D
 
-@export var HP = 30
-@export var DAMAGE = 10
+@export var HP : int = 30
+@export var DAMAGE : int = 10
+@export var goal : Node2D = null
+
+func _ready():
+	assert(goal != null)
 
 func _physics_process(delta):
-	var target = Vector2(150, 500) #TODO update this to "Goal" node
-	var new_position = position.move_toward(target, 50)
-	velocity = new_position - position
+	if goal:
+		var target = goal.position
+		var new_position = position.move_toward(target, 500)
+		velocity = new_position - position
+	else:
+		velocity.x = 0
+		velocity.y = 0
 	move_and_slide()
 
 func take_damage(amount):
 	HP -= amount
 	if HP <= 0:
+		queue_free()
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("take_damage") and body == goal:
+		body.take_damage(DAMAGE)
 		queue_free()
