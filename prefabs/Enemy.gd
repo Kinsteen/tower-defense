201dc5b7f8b297f27a48a97c12f8edfe85f8a4a2
PaulLuperini <paul.luperini@gmail.com>
2023-12-09 16:34:51 +0100

    Add a first version of tileset based pathfinding
    Currently crashes/bugs at wave 3, as enemys collide with each other

--- prefabs/Enemy.gd
+++ prefabs/Enemy.gd
@@ -3,14 +3,21 @@ class_name Enemy extends CharacterBody2D
 @export var HP : int = 30
 @export var DAMAGE : int = 10
 @export var GOAL : Node2D = null
+@export var PATH : Array[Vector2]
 @export var SPEED : int = 50
+
+var current_target : Vector2
 
 func _ready():
 	assert(GOAL != null)
+	assert(PATH != null && PATH.size() > 0)
+	current_target = PATH.pop_front()
 
 func _physics_process(delta):
 	if GOAL:
-		var target = GOAL.global_position
+		if global_position.distance_to(current_target) < 2:
+			current_target = PATH.pop_front()
+		var target = current_target
 		velocity = global_position.direction_to(target) * SPEED
 	else:
 		velocity.x = 0
