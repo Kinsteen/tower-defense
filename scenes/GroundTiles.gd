cb3466475541951f2c8f3401061612c4deeddb25
PaulLuperini <paul.luperini@gmail.com>
2023-12-19 23:32:15 +0100

    Move the TileSelect UI when the mouse moves
    To indicate what tile is currently selected

--- scenes/GroundTiles.gd
+++ scenes/GroundTiles.gd
@@ -2,6 +2,9 @@ extends TileMap
 
 var astar_grid : AStarGrid2D
 
+const GROUND_LAYER = 0
+const UI_LAYER = 1
+
 func _ready():
 	var grid_size = get_used_rect().size
 	astar_grid = AStarGrid2D.new()
@@ -16,10 +19,21 @@ func _ready():
 				x + get_used_rect().position.x,
 				y + get_used_rect().position.y
 			)
-			var tile_data = get_cell_tile_data(0, tile_position)
+			var tile_data = get_cell_tile_data(GROUND_LAYER, tile_position)
 			
 			if tile_data == null or tile_data.get_custom_data("can_walk") == false:
 				astar_grid.set_point_solid(tile_position)
+
+func _process(_delta):
+	_clean_ui_tiles()
+	var selected_tile = local_to_map(to_local(get_global_mouse_position()))
+	set_cell(UI_LAYER, selected_tile, 1, Vector2i(0,0), 0)
+
+func _clean_ui_tiles():
+	var grid_size = get_used_rect().size
+	for x in grid_size.x:
+		for y in grid_size.y:
+			erase_cell(UI_LAYER, Vector2(x,y))
 
 func get_path_to_target(source: Vector2, target: Vector2) -> Array[Vector2]:
 	var map_source = local_to_map(to_local(source))
